9c6908b6f3976e22d02d2c1bf1d61eb1e86f4177
saiyasone <44050824+saiyasone@users.noreply.github.com>
2023-01-18 11:40:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-rock-paper-scissors
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-18xd5w)
---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+interface gamePlay {
+  label: string;
+  icon: string;
+}
+
+import { Component, VERSION } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  playerScore = 0;
+  comScore = 0;
+  winner = '';
+  player: gamePlay = {
+    icon: '',
+    label: '',
+  };
+  gameQuiz: gamePlay[] = [
+    {
+      label: 'rock',
+      icon: 'fas fa-hand-fist',
+    },
+    {
+      label: 'paper',
+      icon: 'fas fa-hand',
+    },
+    {
+      label: 'scissor',
+      icon: 'fas fa-scissors',
+    },
+  ];
+  playerTwo = this.gameQuiz[0];
+
+  randomPlayer() {
+    if (!this.player.label) {
+      return;
+    }
+    let counter = 0;
+    let startGame = setInterval(() => {
+      counter++;
+      this.playerTwo =
+        this.gameQuiz[Math.floor(Math.random() * this.gameQuiz.length)];
+      this.winner = '';
+      if (counter > 5) {
+        clearInterval(startGame);
+        this.selectWinner();
+      }
+    }, 100);
+  }
+
+  selectAnswer(val: gamePlay) {
+    this.player = val;
+    this.winner = '';
+  }
+
+  selectWinner() {
+    let randomPlayer = this.playerTwo.label;
+    let userPlay = this.player.label;
+    if (
+      (userPlay === 'paper' && randomPlayer === 'rock') ||
+      (userPlay === 'scissor' && randomPlayer === 'paper') ||
+      (userPlay === 'rock' && randomPlayer === 'scissor')
+    ) {
+      this.playerScore += 1;
+      this.winner = 'You win';
+    } else if (
+      (userPlay === 'paper' && randomPlayer === 'paper') ||
+      (userPlay === 'rock' && randomPlayer === 'rock') ||
+      (userPlay === 'scissor' && randomPlayer === 'scissor')
+    ) {
+      this.winner = 'We draw';
+    } else {
+      this.comScore += 1;
+      this.winner = 'You lose';
+    }
+  }
+}
